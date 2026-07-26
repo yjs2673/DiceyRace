69baead46338a3feeac9e3620273b575ac5f7be7
Yun Junseo <yjs2673@naver.com>
2026-07-26 17:56:49 +0900

    Delete Font Asset

--- Assets/Scripts/Dice/Dice.cs
+++ Assets/Scripts/Dice/Dice.cs
@@ -15,7 +15,7 @@ public class Dice : ScriptableObject
     public string diceName;
     public Sprite icon;
     public int price;
-    // public int minValue;
+    // public int minValue;  
     // public int maxValue;
     [TextArea]
     public string description;
