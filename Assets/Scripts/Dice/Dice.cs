019b0f1eec654387e3f212ee7fb6ad84dafb271a
Yun Junseo <yjs2673@naver.com>
2026-07-26 17:43:59 +0900

    Shop: 재화 구분

--- Assets/Scripts/Dice/Dice.cs
+++ Assets/Scripts/Dice/Dice.cs
@@ -15,8 +15,9 @@ public class Dice : ScriptableObject
     public string diceName;
     public Sprite icon;
     public int price;
-    public int minValue;
-    public int maxValue;
+    // public int minValue;
+    // public int maxValue;
+    [TextArea]
     public string description;
 
     [Header("Runtime Roll")]
