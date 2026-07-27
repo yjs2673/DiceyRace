9cddb2584712fe49afcc63dd149727b80d28feb2
asdas03 <hongraecob@gmail.com>
2026-07-27 09:36:46 +0900

    0936

--- Assets/Scripts/Shop/InfoPanelUI.cs
+++ Assets/Scripts/Shop/InfoPanelUI.cs
@@ -5,7 +5,6 @@ public class InfoPanelUI : MonoBehaviour
 {
     [SerializeField] private TMP_Text coinText;
     [SerializeField] private TMP_Text pirateCoinText;
-    [SerializeField] private TMP_Text rerollLeftText;
 
     private void Start()
     {
@@ -15,12 +14,10 @@ public class InfoPanelUI : MonoBehaviour
     {
         if (GameManager.Instance == null)
         {
-            Debug.Log("GameManger�� �������� �ʽ��ϴ�.");
             return;
         }
         coinText.text = GameManager.Instance.Coin.ToString();
         pirateCoinText.text = GameManager.Instance.PirateCoin.ToString();
-        rerollLeftText.text = GameManager.Instance.Reroll.ToString();
     }
 
 }
